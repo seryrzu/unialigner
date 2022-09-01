287cda59f6efd31d1eb94a025b6bef34fd606791
seryrzu <seryrzu@gmail.com>
2022-09-01 12:45:10 -0700

    improve cigar visualizer

--- py/visualize_cigar.py
+++ py/visualize_cigar.py
@@ -6,32 +6,43 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
+from utils.bio import parse_cigar
 from utils.os_utils import expandpath, smart_makedirs
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input")
     parser.add_argument("-o", "--output")
+    parser.add_argument("--asm1-name", default="")
+    parser.add_argument("--asm2-name", default="")
+    parser.add_argument("--legend", default="")
     params = parser.parse_args()
 
-    cx, cy = 0, 0
-    x, y = [cx], [cy]
-    with open(params.input) as f:
-        for line in f:
-            mode, len = line.split()
-            len = int(len)
-            if mode == 'M' or mode == 'S':
-                cx += len
-                cy += len
-            elif mode == 'I':
-                cx += len
-            elif mode == 'D':
-                cy += len
 
-            x.append(cx)
-            y.append(cy)
+    for input_file in params.input.split(','):
+        cx, cy = 0, 0
+        x, y = [cx], [cy]
+        with open(input_file) as f:
+            parsed_cigar = parse_cigar(f.readline())[0]
+            for len, mode in parsed_cigar:
+                len = int(len)
+                if mode == 'M' or mode == 'S':
+                    cx += len
+                    cy += len
+                elif mode == 'I':
+                    cy += len
+                elif mode == 'D':
+                    cx += len
 
-    plt.plot(x, y, 'bo-')
+                x.append(cx)
+                y.append(cy)
+
+        plt.plot(x, y)
+
+    if params.legend:
+        plt.legend(params.legend.split(','))
+    plt.xlabel(params.asm1_name)
+    plt.ylabel(params.asm2_name)
     plt.savefig(os.path.join(params.output, 'cigar.pdf'), format='pdf')
 
 main()
