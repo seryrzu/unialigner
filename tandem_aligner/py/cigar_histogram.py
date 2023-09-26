7da2cf2598e72f5720181b0bc650af991fcfe919
IshaanSD <i3gupta@ucsd.edu>
2023-09-26 06:49:00 -0700

    log axes option, outdir, insertion-deletion stats separate

--- tandem_aligner/py/cigar_histogram.py
+++ tandem_aligner/py/cigar_histogram.py
@@ -23,16 +23,29 @@ def plot_hist(params):
                     i += length
         # mode_name = {"M": "Match", "X": "Mismatch", "I": "Insertion", "D": "Deletion"}
         mode_name = {"I": "Insertion", "D": "Deletion"}
+        max_len = max([len for len, pos in stats[mode] for mode in mode_name])
         for m, mode in mode_name.items():
+            print(
+                f"{len(stats[m])} {mode} blocks account for {sum([len for len,pos in stats[m]])} {mode.lower()}s"
+            )
             plt.hist(
-                [x[0] for x in stats[m]], bins=np.arange(0, 20000 + 200, 200), alpha=0.8
+                [x[0] for x in stats[m]],
+                bins=np.arange(0, max_len + params.bin_size, params.bin_size),
+                alpha=0.5,
             )
         # plt.title(mode)
-        plt.xlabel("Length")
+        plt.xlabel("Length (bp)")
         plt.ylabel("Count")
         # plt.xlim(0, 20000)
         plt.legend(list(mode_name.values()))
-        plt.savefig(os.path.join(params.output, input_file + "_hist.pdf"), format="pdf")
+        if params.ylog:
+            plt.yscale("log")
+        if params.xlog:
+            plt.xscale("log")
+        plt.savefig(
+            os.path.join(params.outdir, input_file.split("/")[-1][:-3] + "_hist.pdf"),
+            format="pdf",
+        )
         plt.close()
         if params.hor_len:
             hor_len = params.hor_len
@@ -70,7 +83,9 @@ def plot_hist(params):
             plt.ylabel("HOR-indel multiplicity")
             plt.xlabel("cenX-1 (Mb)")
             plt.savefig(
-                os.path.join(params.output, input_file + "_hor_indels.pdf"),
+                os.path.join(
+                    params.outdir, input_file.split("/")[-1][:-3] + "_hor_indels.pdf"
+                ),
                 format="pdf",
             )
 
@@ -81,44 +96,95 @@ def est_mism_shortindel_rate(params):
             parsed_cigar, cnt = parse_cigar(f.readline())
 
         norm_alignment_len = 0
-        n_mism, n_short_indels, n_long_indels = 0, 0, 0
-        len_mism, len_short_indels, len_long_indels = 0, 0, 0
-        max_indel = 0
+        (
+            n_mism,
+            n_short_insertions,
+            n_short_deletions,
+            n_long_insertions,
+            n_long_deletions,
+        ) = (0, 0, 0, 0, 0)
+        (
+            len_mism,
+            len_short_insertions,
+            len_short_deletions,
+            len_long_insertions,
+            len_long_deletions,
+        ) = (0, 0, 0, 0, 0)
+        max_insertion, max_deletion = 0, 0
         for length, mode in parsed_cigar:
             if mode == "X":
                 n_mism += 1
                 len_mism += length
-            if mode in "ID":
+            if mode == "I":
                 if length > params.max_short_indel_len:
-                    n_long_indels += 1
-                    len_long_indels += length
-                    max_indel = max(max_indel, length)
+                    n_long_insertions += 1
+                    len_long_insertions += length
+                    max_insertion = max(max_insertion, length)
                     continue
                 else:
-                    n_short_indels += 1
-                    len_short_indels += length
+                    n_short_insertions += 1
+                    len_short_insertions += length
+            if mode == "D":
+                if length > params.max_short_indel_len:
+                    n_long_deletions += 1
+                    len_long_deletions += length
+                    max_deletion = max(max_deletion, length)
+                    continue
+                else:
+                    n_short_deletions += 1
+                    len_short_deletions += length
             norm_alignment_len += length
 
         print("norm_alignment_len = ", norm_alignment_len)
         print("n_mism = ", n_mism, "len_mism = ", len_mism)
         print(
-            "n_short_indels = ", n_short_indels, "len_short_indels = ", len_short_indels
+            "n_short_insertions = ",
+            n_short_insertions,
+            "len_short_indels = ",
+            len_short_insertions,
         )
-        print("n_long_indels = ", n_long_indels, "len_long_indels = ", len_long_indels)
+        print(
+            "n_long_insertions = ",
+            n_long_insertions,
+            "len_long_indels = ",
+            len_long_insertions,
+        )
+        print(
+            "n_short_deletions = ",
+            n_short_deletions,
+            "len_short_indels = ",
+            len_short_deletions,
+        )
+        print(
+            "n_long_deletions = ",
+            n_long_deletions,
+            "len_long_indels = ",
+            len_long_deletions,
+        )
         print("len_mism / norm_alignment_len", len_mism / norm_alignment_len)
         print(
-            "len_short_indels / norm_alignment_len",
-            len_short_indels / norm_alignment_len,
+            "len_short_insertions / norm_alignment_len",
+            len_short_insertions / norm_alignment_len,
         )
-        print("max_indel", max_indel)
+        print("len_long_insertions", len_long_insertions)
+        print(
+            "len_short_deletions / norm_alignment_len",
+            len_short_deletions / norm_alignment_len,
+        )
+        print("len_long_deletions", len_long_deletions)
+        print("max_insertion", max_insertion)
+        print("max_deletion", max_deletion)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input")
-    parser.add_argument("-o", "--output")
+    parser.add_argument("-o", "--outdir")
     parser.add_argument("-l", "--hor-len", type=int)
     parser.add_argument("-m", "--max-short-indel-len", type=int, default=5)
+    parser.add_argument("--xlog", action="store_true")
+    parser.add_argument("--ylog", action="store_true")
+    parser.add_argument("--bin_size", type=int, default=200)
     params = parser.parse_args()
 
     plot_hist(params)
@@ -134,7 +200,7 @@ def main():
     #         pos += length
 
     # plt.hist(pos_mism, bins=100)
-    # plt.savefig(os.path.join(params.output, input_file + "_mism_hist.pdf"), format="pdf")
+    # plt.savefig(os.path.join(params.outdir, input_file + "_mism_hist.pdf"), format="pdf")
     # plt.close()
 
     # pos_short_indel = []
@@ -147,7 +213,7 @@ def main():
     #         pos += length
 
     # plt.hist(pos_short_indel, bins=100)
-    # plt.savefig(os.path.join(params.output, input_file + "_smallindel_hist.pdf"), format="pdf")
+    # plt.savefig(os.path.join(params.outdir, input_file + "_smallindel_hist.pdf"), format="pdf")
     # plt.close()
 
 
